c0171c00485fbcda49e8c814ca03fa8370dbd068
zoubeidaNM <zoumenz@yahoo.com>
2017-10-26 16:40:20 -0400

    Updated to add limits to the Resume Items.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Resume resume = new Resume();
-
         Scanner keyboard = new Scanner(System.in);
 
+        Resume resume = new Resume();
+
+        // get resume Max in Nin numbers
+        int maxEdExp = resume.getEdExpMaxNumber();
+        int minEdExp = resume.getEdExpMinNumber();
+        int maxJobExp = resume.getJobExpMaxNumber();
+        int minJobExp = resume.getJobExpMinNumber();
+        int minSkill = resume.getSkillMinNumber();
+        int maxSkill = resume.getSkillMaxNumber();
+
+        //answer from the user
         String answer = "";
+
+        writeWelcomeMessage(resume);
 
         // enter name and email address
         System.out.print("Enter name:");
@@ -20,85 +31,200 @@ public class Main {
         resume.setEmail(keyboard.nextLine());
 
         //enter the educational experiences
+        /*-------------------------------------------------------------------------------------------------------------------------*/
         do {
-
-            System.out.print("Press (E) to enter an educational experience or (Q) to quit.");
+            System.out.println("\nYou have "+(char)27 + "[1m" +resume.getEducationalExpNumber()+  (char)27 +"[0m educational experiences entred.");
+            if(minEdExp != 0 && (resume.getEducationalExpNumber() < minEdExp)) {
+                System.out.println("You have to enter a minimum of " +(char)27 + "[1m"+ minEdExp + (char)27 +"[0m educational experiences.");
+            }
+            System.out.print("\nPress (E) to enter an educational experience or (Q) to quit.");
             answer = keyboard.next();
             keyboard.nextLine();
-            if (answer.equalsIgnoreCase("e")) {
-                EducationalExperience educationalExperience = new EducationalExperience();
-                System.out.print("Enter school name:");
-                educationalExperience.setSchoolName(keyboard.nextLine());
-                System.out.print("Enter degree:");
-                educationalExperience.setDegree(keyboard.nextLine());
-                System.out.print("Enter year of completion:");
-                educationalExperience.setYearCompletion(keyboard.nextLine());
 
-                resume.addResumeItem(educationalExperience);
+            if (answer.equalsIgnoreCase("e")) {
+                //enter educational experience
+                if (resume.getEducationalExpNumber() < maxEdExp) {
+                    //add educational experience if the number of educational experiences is between minEdExp and maxEdExp
+                    addEducationalExperience(resume);
+                } else {
+                    // display a message if the user reached the maxEdExp number of educational experiences and quit
+                    System.out.println((char)27 +"[31m\n***You can only enter a maximum of " +(char)27 +"[1m"+ maxEdExp + (char)27 +"[31m educational experiences.");
+                    System.out.println((char)27 +"[1mPress (Enter) to continue."+(char)27 +"[0m");
+                    keyboard.nextLine();
+                    answer = "Q";
+                }
+            } else if (answer.equalsIgnoreCase("Q")) {
+                //check if the user entered  the minEdExp number of educational experiences
+                if (resume.getEducationalExpNumber() < minEdExp) {
+                    // display a message if the user did not reach the minEdExp number of educational experiences
+                    System.out.println((char)27 + "[31m***\nYou have to enter at least " +(char)27 + "[1m"+ minEdExp + (char)27 + "[31m educational experience");
+                    System.out.println("Press (Enter) to continue."+(char)27 + "[0m");
+                    keyboard.nextLine();
+                    answer = "No Quit";
+                }
             }
         } while (!answer.equalsIgnoreCase("q"));
 
         //enter job experiences
+        /*-------------------------------------------------------------------------------------------------------------------------*/
         do {
+            System.out.println("\nYou have "+ (char)27 + "[1m"+ resume.getJobExpNumber()+(char)27 + "[0m job experiences entred.\n");
+            if(minJobExp != 0) {
+                System.out.println("You have to enter a minimum of " +(char)27 + "[1m"+ minJobExp + (char)27 + "[0m job experiences.\n");
+            }
             System.out.print("Press (J) to enter an job experience or (Q) to quit.");
-            int dutyCounter = 0;
             answer = keyboard.next();
             keyboard.nextLine();
             if (answer.equalsIgnoreCase("j")) {
-                JobExperience jobExperience = new JobExperience();
-                System.out.print("Enter titleOfPosition:");
-                jobExperience.setTitleOfPosition(keyboard.nextLine());
-                System.out.print("Enter job place:");
-                jobExperience.setPlace(keyboard.nextLine());
-                System.out.print("Enter start date:");
-                jobExperience.setStartDate(keyboard.nextLine());
-                System.out.print("Enter end date:");
-                jobExperience.setEndDate(keyboard.nextLine());
-
-                // enter duties
-                String answer2 = "";
-                do {
-                    System.out.print("   Press (D) to enter a duty or (Q) to quit.");
-                    answer2 = keyboard.next();
+                if (resume.getJobExpNumber() < maxJobExp) {
+                    //add a job experience if the number of job experiences is between minJobExp and maxJobExp
+                    addJobExperience(resume);
+                } else {
+                    // display a message if the user reached the maxJobExp number of job experiences and quit
+                    System.out.println((char)27 +"[31m\n***You can only enter a maximum of " +(char)27 +"[1m"+ maxJobExp + (char)27 +"[31m job experiences.");
+                    System.out.println((char)27 +"[1mPress (Enter) to continue."+(char)27 +"[0m");
                     keyboard.nextLine();
-                    if (answer2.equalsIgnoreCase("D")) {
-
-                        System.out.print("   Enter a duty:");
-                        dutyCounter++;
-                        Duty duty = new Duty("Duty" + dutyCounter, keyboard.nextLine());
-
-                        jobExperience.addDuty(duty);
-                    }
-                } while (!answer2.equalsIgnoreCase("q"));
-
-                resume.addResumeItem(jobExperience);
+                    answer = "Q";
+                }
+            } else if (answer.equalsIgnoreCase("Q")) {
+                //check if the user entered  the minJobExp number of job experiences
+                if (resume.getJobExpNumber() < minJobExp) {
+                    // display a message if the user did not reach the minJobExp number of job experiences
+                    System.out.println((char)27 + "[31m***\nYou have to enter at least " +(char)27 + "[1m"+ minJobExp + (char)27 + "[31m job experience");
+                    System.out.println("Press (Enter) to continue."+(char)27 + "[0m");
+                    keyboard.nextLine();
+                    answer = "No Quit";
+                }
             }
-
         } while (!answer.equalsIgnoreCase("q"));
 
         //enter skills
+        /*-------------------------------------------------------------------------------------------------------------------------*/
         do {
-            System.out.print("Press (S) to enter a skill or (Q) to quit.");
+            System.out.println("\nYou have "+ (char)27 +"[1m"+ resume.getSkillsNumber()+(char)27 +"[0m skills entred.");
+            if(minSkill != 0) {
+                System.out.println("You have to enter a minimum of " + (char)27 +"[1m"+resume.getSkillMinNumber() + (char)27 +"[0m skills.");
+            }
+            System.out.println("\nPress (S) to enter a skill or (Q) to quit.");
             answer = keyboard.next();
             keyboard.nextLine();
+
             if (answer.equalsIgnoreCase("s")) {
-                Skill skill = new Skill();
-                System.out.print("Enter skill name:");
-                skill.setName(keyboard.nextLine());
-                System.out.print("Enter skill rating:");
-                skill.setRating(keyboard.nextLine());
-
-                resume.addResumeItem(skill);
+                if (resume.getSkillsNumber() < maxSkill) {
+                    //add a skill if the number of skills is between minSkill and maxSkill
+                    addSkill(resume);
+                } else {
+                    // display a message if the user reached the maxSkill number of skills and quit
+                    System.out.println((char)27 +"[31m\n***You can only enter a maximum of " +(char)27 +"[1m"+ maxSkill + (char)27 +"[31m skills.");
+                    System.out.println((char)27 +"[1mPress (Enter) to continue."+(char)27 +"[0m");
+                    keyboard.nextLine();
+                    answer = "Q";
+                }
+            } else if (answer.equalsIgnoreCase("Q")) {
+                //check if the user entered  the minSkill number of skill
+                if (resume.getSkillsNumber() < minSkill) {
+                    // display a message if the user did not reach the minSkill number of skills
+                    System.out.println((char)27 + "[31m\n***You have to enter at least " +(char)27 + "[1m"+ minSkill + (char)27 + "[31m skills");
+                    System.out.println("Press (Enter) to continue."+(char)27 + "[0m");
+                    keyboard.nextLine();
+                    answer = "No Quit";
+                }
             }
-
-
         } while (!answer.equalsIgnoreCase("q"));
 
-        System.out.println("\n============================================================================");
-        System.out.println("Resume");
-        System.out.println("============================================================================\n");
+        //display the resume
+        displayResume(resume);
+    }
+
+    // write a welcome message to the user
+    public static void writeWelcomeMessage(Resume resume){
+        System.out.println("---------------------------------------------------------------");
+        System.out.println((char)27 +"[1m Welcome to Robo Resume");
+        System.out.println((char)27 + "[0m---------------------------------------------------------------");
+        System.out.println("You have to enter between " + (char)27 + "[1m" + resume.getEdExpMinNumber()+ (char)27 + "[0m and "+
+                (char)27 + "[1m" + resume.getEdExpMaxNumber() + (char)27 +"[0m educational experiences.");
+        System.out.println("You have to enter between " + (char)27 + "[1m" + resume.getJobExpMinNumber()+ (char)27 + "[0m and "+
+                (char)27 + "[1m" + resume.getJobExpMaxNumber() + (char)27 +"[0m job experiences.");
+        System.out.println("You have to enter between " + (char)27 + "[1m" + resume.getSkillMinNumber()+ (char)27 + "[0m and "+
+                (char)27 + "[1m" + resume.getSkillMaxNumber() + (char)27 +"[0m skills.\n");
+    }
+
+    // ask the user to enter the attributes of an EducationalExperience
+    // and add it to the resume
+    public static void addEducationalExperience(Resume resume) {
+        Scanner keyboard = new Scanner(System.in);
+
+        EducationalExperience educationalExperience = new EducationalExperience();
+        System.out.print("\n  Enter school name:");
+        educationalExperience.setSchoolName(keyboard.nextLine());
+        System.out.print("  Enter degree:");
+        educationalExperience.setDegree(keyboard.nextLine());
+        System.out.print("  Enter year of completion:");
+        educationalExperience.setYearCompletion(keyboard.nextLine());
+
+        resume.addResumeItem(educationalExperience);
+
+    }
+
+    // ask the user to enter the attributes of a jobExperience
+    // and add it to the resume
+    public static void addJobExperience(Resume resume) {
+        Scanner keyboard = new Scanner(System.in);
+
+        String answer = "";
+        int dutyCounter = 0;
+
+        JobExperience jobExperience = new JobExperience();
+        System.out.print("  Enter titleOfPosition:");
+        jobExperience.setTitleOfPosition(keyboard.nextLine());
+        System.out.print("  Enter job place:");
+        jobExperience.setPlace(keyboard.nextLine());
+        System.out.print("  Enter start date:");
+        jobExperience.setStartDate(keyboard.nextLine());
+        System.out.print("  Enter end date:");
+        jobExperience.setEndDate(keyboard.nextLine());
+
+        do {
+            System.out.print("   Press (D) to enter a duty or (Q) to quit.");
+            answer = keyboard.next();
+            keyboard.nextLine();
+            if (answer.equalsIgnoreCase("D")) {
+
+                System.out.print("     Enter a duty:");
+                dutyCounter++;
+                Duty duty = new Duty("Duty" + dutyCounter, keyboard.nextLine());
+
+                jobExperience.addDuty(duty);
+            }
+        } while (!answer.equalsIgnoreCase("q"));
+
+        resume.addResumeItem(jobExperience);
+
+    }
+
+    // ask the user to enter the attributes of a skill
+    // and add it to the resume
+    public static void addSkill(Resume resume) {
+        Scanner keyboard = new Scanner(System.in);
+
+        Skill skill = new Skill();
+        System.out.print("  Enter skill name:");
+        skill.setName(keyboard.nextLine());
+        System.out.print("  Enter skill rating:");
+        skill.setRating(keyboard.nextLine());
+
+        resume.addResumeItem(skill);
+
+    }
+
+    // display the resume to the console
+    public static void displayResume(Resume resume) {
+
+        System.out.println((char)27 +"[35m\n============================================================================" +(char)27 + "[0m");
+        System.out.println((char)27 +"[1mResume"+(char)27 + "[0m");
+        System.out.println((char)27 +"[35m============================================================================"+(char)27 + "[0m\n");
         System.out.println(resume);
-        System.out.println("============================================================================");
+        System.out.println((char)27 +"[35m============================================================================"+(char)27 + "[0m");
 
     }
 }
--- src/com/company/Resume.java
+++ src/com/company/Resume.java
@@ -8,6 +8,12 @@ public class Resume {
 
     private String name;
     private String email;
+    private int edExpMaxNumber;
+    private int jobExpMaxNumber;
+    private int skillMaxNumber;
+    private int edExpMinNumber;
+    private int jobExpMinNumber;
+    private int skillMinNumber;
 
     private ArrayList<ResumeItem> resumeItems;
 
@@ -15,6 +21,12 @@ public class Resume {
         name = "";
         email= "";
         resumeItems = new ArrayList<ResumeItem>();
+        edExpMinNumber = 1;
+        edExpMaxNumber = 10;
+        jobExpMinNumber = 0;
+        jobExpMaxNumber = 10;
+        skillMinNumber = 1;
+        skillMaxNumber = 20;
 
     }
 
@@ -23,7 +35,93 @@ public class Resume {
         name = name;
         this.email = email;
         this.resumeItems = resumeItems;
+
+        edExpMinNumber = 1;
+        edExpMaxNumber = 10;
+        jobExpMinNumber = 0;
+        jobExpMaxNumber = 10;
+        skillMinNumber = 1;
+        skillMaxNumber = 20;
     }
+
+
+
+    public int getEducationalExpNumber() {
+        int counter=0;
+        for (ResumeItem resumeItem:resumeItems)
+        {
+            if(resumeItem.getClass().getSimpleName().equals("EducationalExperience")) {
+                counter++;
+            }
+        }
+
+         return counter;
+    }
+
+
+    public int getJobExpNumber() {
+        int counter=0;
+        for (ResumeItem resumeItem:resumeItems)
+        {
+            if(resumeItem.getClass().getSimpleName().equals("JobExperience")) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+
+    public int getSkillsNumber() {
+        int counter=0;
+        for (ResumeItem resumeItem:resumeItems)
+        {
+          if(resumeItem.getClass().getSimpleName().equals("Skill")) {
+                    counter++;
+                }
+        }
+
+        return counter;
+    }
+
+
+   // write the resume in a string
+    public String toString(){
+
+        String ResItemString = "";
+        int counter1 = 0;
+        int counter2 = 0;
+        int counter3 = 0;
+
+        for (ResumeItem resumeItem:resumeItems)
+        {
+
+            if (resumeItem.getClass().getSimpleName().equals("EducationalExperience"))
+            {
+                if(counter1 == 0) {
+                    ResItemString += (char)27 + "[1mEducation"+ (char)27 + "[0m\n";
+                    counter1++;
+                }
+            }else if (resumeItem.getClass().getSimpleName().equals("JobExperience"))
+            {
+                if(counter2 == 0) {
+                    ResItemString += (char)27 + "[1mExperience" + (char)27 + "[0m\n";
+                    counter2++;
+                }
+            }else if(resumeItem.getClass().getSimpleName().equals("Skill")) {
+                if (counter3 == 0) {
+                    ResItemString += (char)27 + "[1mSkills" + (char)27 + "[0m\n";
+                    counter3++;
+                }
+            }
+            ResItemString = ResItemString+ resumeItem +"\n";
+        }
+
+        return  (char)27 + "[1m"+name + (char)27 + "[0m\n"+
+                email + "\n\n" +
+                ResItemString ;
+    }
+
+
 
     public String getName() {
         return name;
@@ -49,45 +147,35 @@ public class Resume {
         this.resumeItems = resumeItems;
     }
 
-    // write the resume in a string
-    public String toString(){
-
-        String ResItemString = "";
-        int counter1 = 0;
-        int counter2 = 0;
-        int counter3 = 0;
-
-        for (ResumeItem resumeItem:resumeItems)
-        {
-
-            if (resumeItem.getClass().getSimpleName().equals("EducationalExperience"))
-            {
-                if(counter1 == 0) {
-                    ResItemString += "Education \n";
-                    counter1++;
-                }
-            }else if (resumeItem.getClass().getSimpleName().equals("JobExperience"))
-            {
-                if(counter2 == 0) {
-                    ResItemString += "Experience \n";
-                    counter2++;
-                }
-            }else if(resumeItem.getClass().getSimpleName().equals("Skill")) {
-                if (counter3 == 0) {
-                    ResItemString += "Skills \n";
-                    counter3++;
-                }
-            }
-            ResItemString = ResItemString+ resumeItem +"\n";
-        }
-
-        return  name + "\n"+
-                email + "\n\n" +
-                ResItemString ;
+    public int getEdExpMaxNumber() {
+        return edExpMaxNumber;
     }
 
+    public int getJobExpMaxNumber() {
+        return jobExpMaxNumber;
+    }
+
+    public int getSkillMaxNumber() {
+        return skillMaxNumber;
+    }
+
+    public int getEdExpMinNumber() {
+        return edExpMinNumber;
+    }
+
+    public int getJobExpMinNumber() {
+        return jobExpMinNumber;
+    }
+
+    public int getSkillMinNumber() {
+        return skillMinNumber;
+    }
 
     public void addResumeItem(ResumeItem resumeItem) {
     resumeItems.add(resumeItem);
     }
+
+
+
+
 }
